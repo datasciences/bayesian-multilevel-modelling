f34af4590682fb8c4b26b845b044968ca26e7dc6
WK Fong <wkfong@WKs-MacBook-Pro.local>
2019-10-11 21:49:32 +0100

    multilevel linear regression classes and demo notebook

--- multilevel_regression.py
+++ multilevel_regression.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from scipy.stats.mstats import mquantiles
 from scipy.stats import boxcox, t
 from scipy.special import inv_boxcox
 import pymc3 as pm
+import copy
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
@@ -29,25 +29,6 @@ class MultiLevelModel(object):
 
     def fit(self, X, y, draws=4000, tune=2000, chains=4, cores=4,
             target_accept=.8, burn=500, **model_kwargs):
-        """
-
-        Parameters
-        ----------
-        X: numpy array
-            Shape should be (n_features, n_observations) e.g. df.values.T
-        y
-        draws
-        tune
-        chains
-        cores
-        target_accept
-        burn
-        model_kwargs
-
-        Returns
-        -------
-
-        """
 
         self.model_ = self._build_model(X=X, y=y, **model_kwargs)
 
@@ -61,6 +42,22 @@ class MultiLevelModel(object):
     def predict(self, X, **kwargs):
         raise NotImplementedError()
 
+    def cv(self, X, y, n_splits, random_state=None, **obj_func_kwargs):
+        folds = KFold(n_splits=n_splits, random_state=random_state)
+        cv_scores_train = np.empty(n_splits)
+        cv_scores_test = np.empty(n_splits)
+
+        for i, (train_idx, test_idx) in enumerate(folds.split(X)):
+
+            cv_scores_train[i], cv_scores_test[i] = \
+                self._objective_func(X, y, train_idx, test_idx, **obj_func_kwargs)
+
+        return cv_scores_train.mean(), cv_scores_train.std(), \
+               cv_scores_test.mean(), cv_scores_test.std()
+
+    def _objective_func(self, X_train, y_train, X_test, y_test, **kwargs):
+        raise NotImplementedError()
+
 
 class PoolLinearModel(MultiLevelModel):
     def _build_model(self, X, y, **kwargs):
@@ -71,7 +68,7 @@ class PoolLinearModel(MultiLevelModel):
             sigma = pm.HalfNormal('sigma', sigma=1e5)
 
             # mean: linear regression
-            mu = alpha + pm.math.dot(beta, X)
+            mu = alpha + beta * X  # alpha + pm.math.dot(beta, X)
 
             # degree of freedom
             nu = pm.Exponential('nu', 1 / 30)
@@ -82,46 +79,102 @@ class PoolLinearModel(MultiLevelModel):
         return model
 
     def predict(self, X, **kwargs):
-        """
-
-        Parameters
-        ----------
-        X: np.array
-            Must be a 2d array (n_observations, n_features)
-        kwargs
-
-        Returns
-        -------
-
-        """
         mu = self.trace_['alpha'] + self.trace_['beta'] * X[:, None]
         dist = t(df=self.trace_['nu'], loc=mu, scale=self.trace_['sigma'])
         if kwargs.get('q') is None:
-            return dist, dist.mean().mean(axis=2)
+            return dist, dist.mean().mean(axis=1)
         else:
-            return dist, [dist.ppf(q_).mean(axis=2) for q_ in kwargs['q']]
+            return dist, [dist.ppf(q_).mean(axis=1) for q_ in kwargs['q']]
+
+    def _objective_func(self, X_train, y_train, X_test, y_test, **kwargs):
+        self.fit(X_train, y_train, **kwargs['fit'])
+
+        _, y_pred_train = self.predict(X_train)
+        _, y_pred_test = self.predict(X_test)
+
+        rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+        rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+
+        return rmse_train, rmse_test
 
 
-class UnpoolModel(MultiLevelModel):
+class PartialPoolLinearModel(MultiLevelModel):
     def _build_model(self, X, y, **kwargs):
         group_idx = kwargs['group_idx']
         n_groups = np.unique(group_idx).shape[0]
-        n_features = X.shape[-1]
+        # n_features = X.shape[0]
 
-        # if self.n_groups_ is None:
-        #     self.n_groups_ = np.unique(group_idx).shape[0]
-        #
-        # if self.n_features_ is None:
-        #     self.n_features_ = X.shape[-1]
+        with pm.Model() as model:
+            # intercept hyper-priors
+            alpha_mu = pm.Normal('alpha_mu', mu=0, sigma=1e5)
+            alpha_sigma = pm.HalfNormal('alpha_sigma', sigma=1e5)
+
+            # intercept prior
+            alpha_t = pm.Normal('alpha_t', mu=0, sigma=1, shape=n_groups)
+            alpha = pm.Deterministic('alpha', alpha_mu + alpha_sigma * alpha_t)
+
+            # slope hyper-priors
+            beta_mu = pm.Normal('beta_mu', mu=0, sigma=1e5)
+            beta_sigma = pm.HalfNormal('beta_sigma', sigma=1e5)
+
+            # slope prior
+            beta_t = pm.Normal('beta_t', mu=0, sigma=1, shape=n_groups)
+            beta = pm.Deterministic('beta', beta_mu + beta_sigma * beta_t)
+
+            # model error
+            sigma = pm.HalfNormal('sigma', sigma=1e5)
+
+            # degree of freedom
+            nu = pm.Exponential('nu', lam=1 / 30.)
+
+            # expected value
+            mu = alpha[group_idx] + beta[group_idx] * X
+
+            # data likelihood
+            pm.StudentT('y', mu=mu, sigma=sigma, nu=nu, observed=y)
+
+        return model
+
+    def predict(self, X, **kwargs):
+        group_idx = kwargs['group_idx']
+        mu = self.trace_['alpha'][:, group_idx] + self.trace_['beta'][:, group_idx] * X[:, None]
+        dist = t(df=self.trace_['nu'], loc=mu, scale=self.trace_['sigma'])
+        if kwargs.get('q') is None:
+            return dist, dist.mean().mean(axis=1)
+        else:
+            return dist, [dist.ppf(q_).mean(axis=1) for q_ in kwargs['q']]
+
+    def _objective_func(self, X, y, train_idx, test_idx, **kwargs):
+        # TODO: make sure group index are assigned properly
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+        kwargs_ = copy.copy(kwargs)
+        kwargs_['fit']['group_idx'] = kwargs['fit']['group_idx'][train_idx]
+        self.fit(X_train, y_train, **kwargs_['fit'])
+
+        _, y_pred_train = self.predict(X_train, **kwargs['predict'])
+        _, y_pred_test = self.predict(X_test, **kwargs['predict'])
+
+        rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+        rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+
+        return rmse_train, rmse_test
+
+
+class UnpoolLinearModel(PartialPoolLinearModel):
+    def _build_model(self, X, y, **kwargs):
+        group_idx = kwargs['group_idx']
+        n_groups = np.unique(group_idx).shape[0]
+        # n_features = X.shape[0]
 
         with pm.Model() as model:
             # priors
-            alpha = pm.Normal('alpha', mu=0, sigma=1e5, shape=(n_features, n_groups))
-            beta = pm.Normal('beta', mu=0, sigma=1e5, shape=(n_features, n_groups))
+            alpha = pm.Normal('alpha', mu=0, sigma=1e5, shape=n_groups)  # shape=(n_groups, n_features)
+            beta = pm.Normal('beta', mu=0, sigma=1e5, shape=n_groups)  # shape=(n_groups, n_features)
             sigma = pm.HalfNormal('sigma', sigma=1e5)
 
             # mean: linear regression
-            mu = alpha[:, group_idx] + pm.math.dot(X, beta[:, group_idx])
+            mu = alpha[group_idx] + beta[group_idx] * X  # alpha[group_idx] + pm.math.dot(beta[group_idx], X)
 
             # degree of freedom
             nu = pm.Exponential('nu', 1 / 30.)
@@ -132,82 +185,14 @@ class UnpoolModel(MultiLevelModel):
         return model
 
 
-class PartialPoolModel(MultiLevelModel):
-    def _build_model(self, X, y, **kwargs):
-        group_idx = kwargs['group_idx']
-        n_groups = self.n_groups if self.n_groups is not None else np.unique(group_idx).shape[0]
-        n_features = X.shape[-1]
-
-        with pm.Model() as model:
-            # intercept hyper-priors
-            alpha_mu = pm.Normal('alpha_mu', mu=0, sigma=1e5)
-            alpha_sigma = pm.HalfNormal('sigma', sigma=1e5)
-
-            # intercept prior
-            alpha = pm.Normal('alpha', mu=alpha_mu, sigma=alpha_sigma, shape=(n_features, n_groups))
-
-            # slope hyper-priors
-            beta_mu = pm.Normal('beta_mu', mu=0, sigma=1e5)
-            beta_sigma = pm.HalfNormal('sigma', sigma=1e5)
-
-            # slope prior
-            beta = pm.Normal('beta', mu=beta_mu, sigma=beta_sigma, shape=(n_features, n_groups))
-
-            # model error
-            sigma = pm.HalfNormal('sigma', sigma=1e5)
-
-            # mean: linear regression
-            mu = alpha[:, group_idx] + pm.math.dot(X, beta[:, group_idx])
-
-            # degree of freedom
-            nu = pm.Exponential('nu', lam=1 / 30.)
-
-            # data likelihood
-            pm.StudentT('y', mu=mu, sigma=sigma, nu=nu, observed=y)
-
-            ###
-            n_groups = county_cat.categories.shape[0]
-
-            # intercept hyper-priors
-            alpha_mu = pm.Normal('alpha_mu', mu=0, sigma=100)
-            alpha_sigma = pm.HalfCauchy('alpha_sigma', beta=5)
-
-            # intercept prior
-            alpha_t = pm.Normal('alpha_t', mu=0, sigma=1, shape=n_groups)
-            alpha = pm.Deterministic('alpha', alpha_mu + alpha_sigma * alpha_t)
-
-            # slope hyper-priors
-            beta_mu = pm.Normal('beta_mu', mu=0, sigma=100)
-            beta_sigma = pm.HalfCauchy('beta_sigma', beta=5)
-
-            # slope prior
-            beta_t = pm.Normal('beta_t', mu=0, sigma=1, shape=n_groups)
-            beta = pm.Deterministic('beta', beta_mu + beta_sigma * beta_t)
-
-            # model error
-            sigma = pm.HalfNormal('sigma', sigma=10 * radon_activity_bc.std())
-
-            # degree of freedom
-            nu = pm.Exponential('nu', lam=1 / 30.)
-
-            # expected value
-            mu = alpha[county_cat_idx] + beta[county_cat_idx] * floor
-
-            # data likelihood
-            y = pm.StudentT('y', mu=mu, sigma=sigma, nu=nu, observed=radon_activity_bc)
-
-        return model
-
-
-def plot_prediction(x, y_mean, y_upper=None, y_lower=None, xlabel=None, ylabel=None,
-                    ax=None, figsize=(18, 6), **kwargs):
+def plot_prediction(x, y_mean, y_upper=None, y_lower=None, xlabel=None,
+                    ylabel=None, ax=None, figsize=(18, 6), **kwargs):
     if ax is None:
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111)
     else:
         fig = ax.get_figure()
 
-    # q = mquantiles(y, prob=prob, axis=0)
     ax.plot(x, y_mean, **kwargs)
     if not (y_upper is None or y_lower is None):
         ax.fill_between(x, y_upper, y_lower, alpha=kwargs.get('alpha', 0.5))
@@ -217,91 +202,3 @@ def plot_prediction(x, y_mean, y_upper=None, y_lower=None, xlabel=None, ylabel=N
     ax.legend()
 
     return fig, ax
-
-
-def cv(func, X, y, n_splits, random_state=None):
-    folds = KFold(n_splits=n_splits, random_state=random_state)
-    cv_scores = np.empty_like(n_splits)
-
-    for i, (train_index, test_index) in enumerate(folds.split(X)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-
-        y_pred = func()
-        cv_scores[i] = np.sqrt(mean_squared_error(y_test, y_pred))
-
-    return cv_scores
-
-
-if __name__ == '__main__':
-    def get_data():
-        # load data
-        srrs2 = pd.read_csv(pm.get_data('srrs2.dat'))
-        srrs2.columns = srrs2.columns.map(str.strip)
-        srrs2['county'] = srrs2['county'].str.strip()
-        srrs_mn = srrs2.loc[srrs2.state == 'MN', :].copy()
-        srrs_mn['fips'] = srrs_mn.stfips * 1000 + srrs_mn.cntyfips
-
-        cty = pd.read_csv(pm.get_data('cty.dat'))
-        cty_mn = cty.loc[cty.st == 'MN', :].copy()
-        cty_mn['fips'] = cty_mn.stfips * 1000 + cty_mn.ctfips
-
-        srrs_mn = srrs_mn.merge(cty_mn[['fips', 'Uppm']], on='fips')
-
-        # target (y)
-        y = srrs_mn['activity'].values
-        y_bc, lambda_bc = boxcox(y + 0.1)
-
-        # predictor (x)
-        x = srrs_mn[['floor']].values.T
-
-        # groups
-        group = pd.Categorical(srrs_mn['county'])
-        group_idx = group.codes
-
-        return srrs_mn, x, y, y_bc, lambda_bc, group, group_idx
-
-
-    dataset, x, y, y_bc, lambda_bc, group, group_idx = get_data()
-
-    model = PoolLinearModel()
-    model.fit(x, y_bc, draws=1000, tune=500, chains=2, cores=4, target_accept=.85, burn=100)
-
-    x_ = np.linspace(x.min(), x.max(), 20)[:, None]
-    y_dist, y_pred = model.predict(x_, q=(0.025, 0.5, 0.975))
-
-    fig, ax = plt.subplots(figsize=(7, 6))
-    ax.scatter(x, y_bc)
-    plot_prediction(x_, y_mean=y_pred[1], y_upper=y_pred[0], y_lower=y_pred[2], xlabel='floor',
-                    ylabel='BoxCox(radon activity)', ax=ax, linestyle='--', c='b', label='pooled')
-
-    # model = UnpoolModel()
-    # model.train(x, y_bc, draws=30, tune=30, chains=1, cores=4, target_accept=.85, burn=0, group_idx=group_idx)
-    # x_ = np.linspace(x.min(), x.max(), 50).reshape(-1, 1)
-    # y_pred = model.predict(x_, samples=500, group_idx=np.ones(50, dtype=np.int16) * 4)['y']
-    #
-    # idx = range(0, group.categories.shape[0], 10)
-    # selected_county = group.value_counts().sort_values(ascending=False).iloc[idx].index.tolist()
-    #
-    # fig, ax = plt.subplots(2, 5, figsize=(18, 7), sharex=True, sharey=True)
-    # ax_ = ax.ravel()
-    #
-    # for i, county in enumerate(selected_county):
-    #     # plot observed data points
-    #     mask = dataset['county'] == county
-    #     ax_[i].scatter(x[mask], y_bc[mask])
-    #     j = np.where(group.categories == county)[0][0]
-    #     x = np.linspace(x.min(), x.max(), 50)
-    #
-    #     # compare pooled and unpooled models
-    #     # pooled model
-    #     plot_prediction(x, y_pooled, xlabel='floor', ylabel='BoxCox(radon activity)', ci=True, ax=ax_[i],
-    #                     linestyle='--', c='b', label='pooled')
-    #
-    #     # unpooled model
-    #     y_unpooled = chain_unpooled['alpha'][:, j] + chain_unpooled['beta'][:, j] * x[:, None]
-    #     plot_prediction(x, y_unpooled, xlabel='floor', ylabel='BoxCox(radon activity)', ci=True, ax=ax_[i],
-    #                     linestyle='-.', c='g', label='unpooled')
-
-    fig.tight_layout()
-    plt.show()
